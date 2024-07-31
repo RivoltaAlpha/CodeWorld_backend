392399e0df3b397ca3416319f9b2ffa285d79ce3
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-07-31 08:02:33 +0300

    projects routes

---
+++ src/projects/Prouter.ts
@@ -0,0 +1,18 @@
+import { Hono, Context } from "hono";
+import { CreatingProject, UpdatingProject, DeletingProject, getProject,getProjects,getUserProjects, getProjectsTasks, getTheProjectLogs, } from "./controller"
+// import { zValidator } from "@hono/zod-validator";
+// import { userSchema } from "../validator";
+// import { authenticateAdmin, authenticateBoth } from "../middleware/auth";
+
+
+export const projectRouter = new Hono();
+
+
+projectRouter.get('/projects', getProjects);
+projectRouter.get('/projects/:id', getProject);
+projectRouter.get('/projects-tasks/:id', async(c: Context) => getProjectsTasks (c));
+projectRouter.get('/projects-logs/:id', async(c: Context) => getTheProjectLogs (c));
+projectRouter.get('/projects/:user_id', async(c: Context) => getUserProjects (c));
+projectRouter.post('/projects', async(c: Context) => CreatingProject (c));
+projectRouter.put('/projects/:id', async(c: Context) => UpdatingProject(c));
+projectRouter.delete('/projects/:id', async(c: Context) => DeletingProject(c))
