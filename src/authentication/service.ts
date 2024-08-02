c4a8cb55e25d20542611d5605a3893bb616baab7
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-02 21:03:30 +0300

    login flow

--- src/authentication/service.ts
+++ src/authentication/service.ts
@@ -1,34 +1,28 @@
 import  db  from '../drizzle/db';
 import { sql} from 'drizzle-orm';
-import {  users } from "../drizzle/schema";
-import { TSAuthentication,authentication } from '../drizzle/schema';
+import {  users, TSUser, TIUser } from "../drizzle/schema";
+import { hash } from 'bcrypt';
 
-
-export const loginAuthService = async (user: TSAuthentication) => {
+export const loginAuthService = async (user:TSUser ) => {
   const { username } = user;
   try {
-    const foundUser = await db.query.authentication.findFirst({
+    const foundUser = await db.query.users.findFirst({
       columns: {
+        user_id: true,
         username: true,
         password: true,
-        role: true, 
-      },where: sql` ${authentication.username} = ${username}`,
-      with: {
-        user: {
-          columns: {
-            user_id: true,
-            first_name: true,
-            last_name: true,
-            username: true,
-            contact_phone: true,
-            address: true,
-            email: true,
-            image_url: true,
-          }
-        }
-      }
+        email: true,
+        image_url: true,
+        role: true,
+      },
+      where: sql` ${users.username} = ${username}`
     });
     console.log('Found user:', foundUser);
+
+    if (!foundUser) {
+        throw new Error('User not found');
+      }
+
     return foundUser;
   } catch (error) {
     console.error('Error logging in user:', error);
@@ -36,33 +30,25 @@ export const loginAuthService = async (user: TSAuthentication) => {
   }
 };
 
-export const authenticationService = async (user: any) => {
+export const authenticationService = async (user: TIUser) => {
   try {
-    console.log('Registering user:', user); // Log received user data for debugging
+    // Log received user data for debugging
+    console.log('Registering user:', user);
+
+    const hashedPassword = await hash(user.password, 10);
 
     // Insert user into `users` table
     const createdUser = await db.insert(users).values({
-      user_name: user.username,
+      username: user.username,
       email: user.email,
+      password: hashedPassword,
+      image_url: user.image_url || null,
       role: user.role || 'user',
-      created_at: sql`now()`,
+      date_joined: sql`now()`,
       updated_at: sql`now()`
     }).returning();
 
     console.log('User created in users table:', createdUser); // Log created user data
-
-    // Extract the created user ID
-    const userId = createdUser[0].user_id;
-
-    // Insert user into `authentication` table
-    const createdAuthUser = await db.insert(authentication).values({
-      user_id: userId,
-      password: user.password,
-      username: user.username,
-      role: user.role || 'user'
-    }).returning();
-
-    console.log('User created in authentication table:', createdAuthUser); // Log created auth user data
 
     return createdUser[0]; // Return the created user
   } catch (error) {
